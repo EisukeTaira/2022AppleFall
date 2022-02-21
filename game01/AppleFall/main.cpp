9b36b37b1a1de9c7823c8d445a729cfd8119950a
EisukeTaira <eisuke-t@kbcgroup.ac.jp>
2022-02-21 14:01:42 +0900

    プロジェクト作成

---
+++ game01/AppleFall/main.cpp
@@ -0,0 +1,21 @@
+#include "DxLib.h"
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
+{
+	ChangeWindowMode(TRUE);
+
+	if (DxLib_Init() == -1) {	// DXライブラリ初期化処理
+		return -1;				// 初期化失敗
+	}
+	SetDrawScreen(DX_SCREEN_BACK);
+	SetGraphMode(640, 480, 32);
+	
+
+	while (ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0) {
+	
+	}
+
+	DxLib_End();	// DXライブラリ使用の終了処理
+
+	return 0;		// ソフトの終了
+}
