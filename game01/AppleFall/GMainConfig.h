9bf1c144be6213afbc6c57ce237d14c8d1f977fa
EisukeTaira <eisuke-t@kbcgroup.ac.jp>
2022-03-08 10:59:23 +0900

    少し変更

--- game01/AppleFall/GMainConfig.h
+++ game01/AppleFall/GMainConfig.h
@@ -8,4 +8,17 @@
 #define	BACK_GROUND_EXT	(0.65)		// 背景画像拡大率
 
 
+
+// 型定義
+typedef void*	T_FUNC;
+
+typedef struct {
+	int		x, y;		// 座標
+	int		w, h;		// スケール
+	float	angle;		// z軸回転
+	int		img;		// 画像
+	bool	flg;		// true:表示	false:非表示
+	T_FUNC	hitcheck;	// 当たり判定チェック
+}T_PLAYER;
+
 #endif // !GAME_MAIN_CONFIG_H__
--- game01/AppleFall/GameMain.cpp
+++ game01/AppleFall/GameMain.cpp
@@ -7,18 +7,27 @@
 
 
 // 変数定義
-int GMainImg[3] = {};	// 画像用変数
-int GMainSnd[3] = {};	// 画像用変数
+int GMainImg[3] = {};							// 画像用変数
+int GMainSnd[3] = {};							// 音源用変数
+T_PLAYER Player;								// プレイヤー
+
+// プロトタイプ宣言
+void stage_background(void);					// 背景描画処理
+void stage_ui_ctrl(void);						// UI描画処理
+int hitcheck(int px, int py, int ex, int ey);	// 当たり判定
 
 // ゲームメイン機能初期化処理
 void GMainInit(void)
 {
-	
+	Player.x = 0;
+	Player.y = 0;
+	Player.hitcheck = &hitcheck;
 }
 // ゲームメイン機能更新処理
 void GMainUpdate(void)
 {
-	DrawRotaGraph(BG_POS_X, BG_POS_Y, 0.65, 0, GMainImg[BACK_GROUND], TRUE);
+	stage_background();
+	
 }
 // ゲームメイン機能画像読み込み処理
 int  LoadGMainImg(void)
@@ -38,4 +47,26 @@ int  LoadGMainImg(void)
 int  LoadGMainSnd(void)
 {
 	return 0;
+}
+// 背景描画処理
+void stage_background(void)
+{
+	//描画可能エリアを認定
+	SetDrawArea(0, 0, 500, 480);
+	// 背景画像の描画
+	DrawRotaGraph(BG_POS_X, BG_POS_Y, 0.65, 0, GMainImg[BACK_GROUND], TRUE);
+
+	//エリアを戻す
+	SetDrawArea(0, 0, 640, 480);
+	//	//ステージ背景
+	DrawBox(500, 0, 640, 480, 0xFF66FF, TRUE);
+}
+// UI描画処理
+void stage_ui_ctrl(void)
+{
+}
+// 当たり判定チェック
+int hitcheck(int px, int py, int ex, int ey)
+{
+	return FALSE;
 }
