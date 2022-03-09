dde7a86385c7217eb6d25b7f9fcc4b3adee402ad
EisukeTaira <eisuke-t@kbcgroup.ac.jp>
2022-03-09 11:23:08 +0900

    playerとApple作成

--- game01/AppleFall/GMainConfig.h
+++ game01/AppleFall/GMainConfig.h
@@ -1,24 +1,35 @@
 #ifndef GAME_MAIN_CONFIG_H__
 
 // 画像配列添え字
-#define	BACK_GROUND		(0)		// 背景画像
+#define	BACK_GROUND		(0)			// 背景画像
 
-#define	BG_POS_X	(640 >> 1)		// 背景画像：X座標
-#define	BG_POS_Y	(480 >> 1)		// 背景画像：Y座標
+#define	BG_POS_X		(640 >> 1)	// 背景画像：X座標
+#define	BG_POS_Y		(480 >> 1)	// 背景画像：Y座標
 #define	BACK_GROUND_EXT	(0.65)		// 背景画像拡大率
 
-
-
+#define	APPLE_MAX		(8)			// リンゴ最大数
+#define TYPE_MAX		(3)			// オブジェクトタイプ数
 // 型定義
 typedef void*	T_FUNC;
-
 typedef struct {
-	int		x, y;		// 座標
-	int		w, h;		// スケール
-	float	angle;		// z軸回転
-	int		img;		// 画像
-	bool	flg;		// true:表示	false:非表示
-	T_FUNC	hitcheck;	// 当たり判定チェック
+	int x;
+	int y;
+}T_Vector2;
+typedef struct {
+	T_Vector2	pos;		// ポジション
+	T_Vector2	scale;		// スケール
+	float		angle;		// z軸回転
+	int			img;		// 画像
+	bool		flg;		// true:表示	false:非表示
 }T_PLAYER;
+typedef struct {
+	T_Vector2	pos;		// ポジション
+	T_Vector2	scale;		// スケール
+	float		angle;		// z軸回転
+	int			type;		// オブジェクトタイプ
+	int			img;		// 画像
+	bool		flg;		// true:表示	false:非表示
+	T_FUNC		hitcheck;	// 当たり判定チェック
+}T_ENEMY;
 
 #endif // !GAME_MAIN_CONFIG_H__
--- game01/AppleFall/GameMain.cpp
+++ game01/AppleFall/GameMain.cpp
@@ -10,7 +10,7 @@
 int GMainImg[3] = {};							// 画像用変数
 int GMainSnd[3] = {};							// 音源用変数
 T_PLAYER Player;								// プレイヤー
-
+T_ENEMY Apple[APPLE_MAX] = {};							// リンゴ						
 // プロトタイプ宣言
 void stage_background(void);					// 背景描画処理
 void stage_ui_ctrl(void);						// UI描画処理
@@ -19,9 +19,27 @@ int hitcheck(int px, int py, int ex, int ey);	// 当たり判定
 // ゲームメイン機能初期化処理
 void GMainInit(void)
 {
-	Player.x = 0;
-	Player.y = 0;
-	Player.hitcheck = &hitcheck;
+	int i;
+
+	Player.pos.x = 0;
+	Player.pos.y = 0;
+	Player.scale.x = 0;
+	Player.scale.y = 0;
+	Player.angle = 0.0f;
+	Player.img = 0;
+	Player.flg = false;
+	for (i = 0; i < APPLE_MAX; i++)
+	{
+		Apple[i].pos.x = 0;
+		Apple[i].pos.y = 0;
+		Apple[i].scale.x = 0;
+		Apple[i].scale.y = 0;
+		Apple[i].angle = 0.0f;
+		Apple[i].type = GetRand(TYPE_MAX - 1);
+		Apple[i].img = 0;
+		Apple[i].flg = false;
+		Apple[i].hitcheck = &hitcheck;
+	}
 }
 // ゲームメイン機能更新処理
 void GMainUpdate(void)
